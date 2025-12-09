56c1f3b8340133d325574c07418fda862f17c0a1
z-erik <erikp2913@gmail.com>
2025-12-09 01:11:19 -0600

    feat: finished

--- exercise6.js
+++ exercise6.js
@@ -2,17 +2,20 @@
 
 let array=[3,6,12,5,100,1];
 
-console.log(array)
+console.log(`before the bubble algorithm`);
+console.log(array);
 
 bubble(array);
+console.log("\n")
 console.log(array);
 
 
 
 function bubble (array){
 let aux;
+console.log("\nshow the iteration's changes 𓁆 \n")
 for (let i = 0; i < array.length; i++) {
-    
+    console.log(`iteration i=${i} 𓇑 |${array.join("|")}|`)
     for (let j = 0; j < array.length-1; j++) {
         
         if(array[j]>array[j+1]){
